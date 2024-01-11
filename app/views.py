1d6aadcb107f082e9759f2ace7577038ff3af372
papaiahgari <pudhy64@gmail.com>
2024-01-11 11:15:00 +0530

    sample_project

---
+++ app/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def two(render):
+    return HttpResponse('this my nwe project')
+def twoo(request):
+    return HttpResponse('this year my rekha project')
